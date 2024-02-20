19c0915c1b3caad19bafdaa75e32d7e2fa09bdee
vladost7 <117936852+vladost7@users.noreply.github.com>
2024-02-21 03:40:20 +0600

    Add files via upload

--- spring-reg-auth-master/src/main/java/com/boots/controller/RegistrationController.java
+++ spring-reg-auth-master/src/main/java/com/boots/controller/RegistrationController.java
@@ -21,7 +21,6 @@ public class RegistrationController {
     @GetMapping("/registration")
     public String registration(Model model) {
         model.addAttribute("userForm", new User());
-
         return "registration";
     }
 
